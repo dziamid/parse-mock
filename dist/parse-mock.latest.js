2c19ade85bfcb2f3f2e4edbe051264ae8b7e5bee
Dziamid <dziamid@gmail.com>
2015-03-25 14:32:36 +0300

    refactor: user in node (src) or browser (dist)

--- dist/parse-mock.latest.js
+++ dist/parse-mock.latest.js
@@ -1,6 +1,8 @@
-var _ = require('lodash'),
-  sinon = require('sinon'),
-  Parse = require('parse'),
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function (global){
+var _ = (typeof window !== "undefined" ? window._ : typeof global !== "undefined" ? global._ : null),
+  sinon = (typeof window !== "undefined" ? window.sinon : typeof global !== "undefined" ? global.sinon : null),
+  Parse = (typeof window !== "undefined" ? window.Parse : typeof global !== "undefined" ? global.Parse : null),
 
   lastObjectId = 1,
   registeredStubs = [],
@@ -84,3 +86,6 @@ function addDefaultFields(data) {
     updatedAt: new Date()
   });
 }
+
+}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
+},{}]},{},[1]);
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,22 @@
 var gulp = require('gulp'),
   minify = require('gulp-uglify'),
-  concat = require('gulp-concat');
+  concat = require('gulp-concat'),
+  browserify = require('browserify'),
+  source = require('vinyl-source-stream');
 
-gulp.task('build', function () {
-  return gulp.src(['src/**.js'])
-    .pipe(minify({mangle: false}))
-    .pipe(concat('parse-mock.min.js'))
-    .pipe(gulp.dest('dist/'));
+gulp.task('browserify', function () {
+  return browserify('./src/parse-mock.js')
+    .external('lodash')
+    .external('sinon')
+    .external('parse')
+    .bundle()
+    //Pass desired output filename to vinyl-source-stream
+    .pipe(source('parse-mock.latest.js'))
+    // Start piping stream to tasks!
+    .pipe(gulp.dest('./dist/'))
 });
 
+//todo: add minify
+
+gulp.task('build', ['browserify']);
 gulp.task('default', ['build']);
